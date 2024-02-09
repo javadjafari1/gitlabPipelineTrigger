e4066814a947a83479a1ec0550b4667c72649a81
javadjafari <javad.jafari@partsoftware.com>
2024-02-09 13:36:55 +0330

    use version catalog for dependency management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,20 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.compose)
 }
 
 group = "ir.thatsmejavad.pipelinetrigger"
 version = "0.0.1"
 
 dependencies {
-    val voyagerVersion = "1.0.0"
-    val koinVersion = "3.5.3"
-    implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
-    implementation("cafe.adriel.voyager:voyager-screenmodel:$voyagerVersion")
-    implementation("cafe.adriel.voyager:voyager-koin:$voyagerVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.3")
-
-    implementation("io.insert-koin:koin-core:$koinVersion")
-
     implementation(compose.desktop.currentOs)
+    implementation(libs.voyagerNavigator)
+    implementation(libs.voyagerScreenModel)
+    implementation(libs.voyagerKoin)
+    implementation(libs.coroutine)
+    implementation(libs.koin)
 }
 
 compose.desktop {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,11 +15,6 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        kotlin("jvm").version("1.9.20")
-        id("org.jetbrains.compose").version("1.5.10")
-    }
 }
 
 dependencyResolutionManagement {
